e4c81a120ab0da828c0f5d8c994fe84afb7a58a3
Harsh <harsh.mithapara@gmail.com>
2023-11-28 02:13:51 +0530

    added page authorization for addblogs page

--- src/app/(views)/blogs/addblog/page.tsx
+++ src/app/(views)/blogs/addblog/page.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import AddBlogForm from './AddBlogForm'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-const page = () => {
-
-    return (<>
-        <AddBlogForm />
-    </>)
+const page = async () => {
+    const cookieStore = cookies()
+    const supabase = createServerComponentClient({ cookies: () => cookieStore })
+    const { data: { session } } = await supabase.auth.getSession();
+    if (session == null) {
+        return redirect("/")
+    } else {
+        return (<>
+            <AddBlogForm />
+        </>)
+    }
 }
 
 
