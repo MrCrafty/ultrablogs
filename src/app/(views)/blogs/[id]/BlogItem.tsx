0fc58dff9856bba3fc8fa86301a7a1190158b9e7
harsh <hmithapara@horizontal.com>
2023-11-28 16:40:44 +0530

    Added Edit Blog feature

---
+++ src/app/(views)/blogs/[id]/BlogItem.tsx
@@ -0,0 +1,43 @@
+'use client'
+import { BlockNoteEditor } from '@blocknote/core';
+import React, { useEffect, useState } from 'react'
+import { BlockNoteView } from '@blocknote/react';
+import "@blocknote/core/style.css"
+import { cookies } from 'next/headers';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
+
+const BlogItem = (data: any) => {
+    const supabase = createClientComponentClient();
+    const router = useRouter();
+    const [editable, seteditable] = useState(false)
+    const [initialData, setInitialData] = useState(data?.data?.data)
+    let editor = new BlockNoteEditor({
+        initialContent: initialData,
+        editable: editable
+    })
+    async function handleSave() {
+        console.log(editor.topLevelBlocks)
+        await supabase.from("data").update({ data: editor.topLevelBlocks }).eq("id", data?.data?.id).then(() => {
+            setInitialData(editor.topLevelBlocks)
+            seteditable(false);
+        })
+    }
+
+    return (
+        <div>
+            <div className='border-b-2 py-2'>
+                <h1 className='text-5xl text-text-semibold mb-4'>{data?.data?.title}</h1>
+                <div className='flex mb-4'>
+                    <p>Tags: </p>
+                    {data?.data?.categories?.map((item: string, index: number) => <p className={`px-3 text-gray-500 ${index > 0 ? "border-black border-s-[1px]" : ""}`} key={index}>{item}</p>)}
+                </div>
+            </div>
+            <BlockNoteView editor={editor} theme={'light'} className='my-10' />
+            {data?.isEditable ? editable ? <button className='bg-green-200 py-1 px-4' onClick={() => { handleSave() }}>Save</button> : <button className='bg-blue-200 py-1 px-4' onClick={() => { seteditable(true) }}>Edit</button> : ""}
+
+        </div>
+    )
+}
+
+export default BlogItem
--- src/app/(views)/blogs/[id]/page.tsx
+++ src/app/(views)/blogs/[id]/page.tsx
@@ -2,23 +2,25 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
 import React from 'react'
-import BlogItem from '../BlogItem';
-async function getData(id: string) {
+import BlogItem from './BlogItem';
+const page = async ({ params }: { params: { id: string } }) => {
     const cookieStore = cookies();
     const supabase = createServerComponentClient({ cookies: () => cookieStore });
-    const res = await supabase.from("data").select().eq("id", id).limit(1).single();
-    if (res.error) {
-        return res.error.message;
-    } else {
-        return res.data;
+    const user = (await supabase.auth.getSession()).data.session?.user.id;
+
+    async function getData(id: string) {
+        const res = await supabase.from("data").select().eq("id", id).limit(1).single();
+        if (res.error) {
+            return res.error.message;
+        } else {
+            return res.data;
+        }
     }
-}
-const page = async ({ params }: { params: { id: string } }) => {
     const data = await getData(params.id);
     return (
         <div className='text-black container'>
             <div className='w-1/2 mx-auto'>
-                <BlogItem categories={data?.categories} data={data?.data} id={data?.id} title={data?.title} user_id={data?.userid} />
+                <BlogItem data={data} isEditable={user == data?.user_id} />
             </div>
         </div>
     )
