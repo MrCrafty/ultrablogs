5351e3e245f2f936ef8711fa7b0c9fc642d77cd3
HarshMithapara <hmithapara@horizontal.com>
2023-12-13 18:24:25 +0530

    removed bugs

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,5 @@
 "use client";
-import { motion } from "framer-motion";
 import React from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa6";
-import AnimatedTextCharacter from "./AnimatedTextCharacter";
 
 const Home = (data: any) => {
   return <div className="container border-b-[1px] border-black py-10"></div>;
