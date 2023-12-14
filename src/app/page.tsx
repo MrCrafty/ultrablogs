975860dc39a08c8913ddcdf27cc0a03d0525aa19
HarshMithapara <hmithapara@horizontal.com>
2023-12-14 23:28:52 +0530

    added home page grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import Home from "@/components/Home";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Blogsite",
 };
 export default async function Page() {
-  return <>{/* <Home data={user} /> */}</>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,18 @@
 "use client";
 import React from "react";
 
-const Home = (data: any) => {
-  return <div className="container border-b-[1px] border-black py-10"></div>;
+const Home = () => {
+  return (
+    <div className="container mx-auto">
+      <h2 className="text-3xl border-b-2 border-gray-500">Explore Blogs</h2>
+      <div className="grid-flow-col grid-cols-2 grid">
+        <div className="">Grid Col 1</div>
+        <div className="">Grid Col 2</div>
+        <div className="">Grid Col 2</div>
+        <div className="">Grid Col 2</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
