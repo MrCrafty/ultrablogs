953925c21137f10f0a3b50e7f025d8e760840203
Harsh <harsh.mithapara@gmail.com>
2023-12-15 10:09:52 +0530

    Added categories

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -23,6 +23,30 @@ const Home = () => {
           image={"https://asset.brandfetch.io/iddWCn9fUD/idmpMGJHi4.jpeg"}
           title={"IITM DS"}
         />
+        <CategoryTabs
+          image={
+            "https://images.unsplash.com/photo-1631549916768-4119b2e5f926?q=80&w=2079&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
+          title={"Medical"}
+        />
+        <CategoryTabs
+          image={
+            "https://images.unsplash.com/photo-1584735935682-2f2b69dff9d2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
+          title={"Health"}
+        />
+        <CategoryTabs
+          image={
+            "https://images.unsplash.com/photo-1488646953014-85cb44e25828?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
+          title={"Travel"}
+        />
+        <CategoryTabs
+          image={
+            "https://images.unsplash.com/photo-1560520653-9e0e4c89eb11?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
+          title={"Financing"}
+        />
       </div>
     </div>
   );
