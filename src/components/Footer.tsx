b2becc80c919f7a2927ae9a4b804033e8a44a17a
HarshMithapara <hmithapara@horizontal.com>
2023-12-18 14:23:52 +0530

    Resolved UI issues

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between gap-5 container border-t-2 py-5 items-center">
+    <div className="flex justify-between gap-5 container border-t-2 py-5 items-center px-3">
       <p>&#169; www.ultrablogs.in</p>
       <Link href={"/contact"} className="text-lg">
         Contact Us
