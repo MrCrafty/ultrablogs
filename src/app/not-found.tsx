94e4825014faf95942360d867193e970dbe59819
HarshMithapara <hmithapara@horizontal.com>
2023-12-18 15:38:32 +0530

    changes 4040 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,25 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const NotFound = () => {
+  const router = useRouter();
   return (
     <div className="mt-20 px-5 w-full flex-col flex justify-center items-center">
       <h1 className="text-5xl">404</h1>
       <p className="text-lg">Page you are looking for is not available.</p>
       <p className="text-2xl">
         Click here to go{" "}
-        <Link href={"/"} className="text-blue-500">
+        <em
+          className="text-blue-500 cursor-pointer"
+          onClick={() => {
+            router.push("/");
+            router.refresh();
+          }}
+        >
           Home
-        </Link>
+        </em>
       </p>
     </div>
   );
