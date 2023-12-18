02a4bda0e9035199af068f2b96b52d19cb70cce5
HarshMithapara <hmithapara@horizontal.com>
2023-12-18 13:03:56 +0530

    Added Footer and Changed Blogs Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 const DynamicHeader = dynamic(() => import("../components/Header"), {
   loading: () => <Loading />,
 });
+const DynamicFooter = dynamic(() => import("../components/Footer"), {
+  loading: () => <Loading />,
+});
 
 export default function RootLayout({
   children,
@@ -23,10 +26,13 @@ export default function RootLayout({
         <header>
           <DynamicHeader />
         </header>
-        <main className="pt-24 font-Poppins">
+        <main className="pt-24 font-Poppins min-h-screen">
           {children}
           <SpeedInsights />
         </main>
+        <footer>
+          <DynamicFooter />
+        </footer>
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-between gap-5 container border-t-2 py-5 items-center">
+      <p>&#169; www.ultrablogs.in</p>
+      <Link href={"/contact"} className="text-lg">
+        Contact Us
+      </Link>
+    </div>
+  );
+};
+
+export default Footer;
